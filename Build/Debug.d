01ce4e713b153c505d9b8d44bffed8bb7d792aaf
BenJilks <benjyjilks@gmail.com>
2018-09-06 19:02:31 +0000

    Clean up

--- Build/Debug.d
+++
@@ -1,3 +0,0 @@
-Build/Debug.o: Compiler/Src/Debug.cpp Compiler/Include/Debug.hpp
-
-Compiler/Include/Debug.hpp:
--- Build/VM.d
+++
@@ -1,8 +0,0 @@
-Build/VM.o: VM/Src/VM.c VM/Include/VM.h VM/Include/Operations.h \
- VM/Include/VM.h
-
-VM/Include/VM.h:
-
-VM/Include/Operations.h:
-
-VM/Include/VM.h:
